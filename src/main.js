a73ab566f7c618472c6618855878904cebb7d74f
yang <1303733687@qq.com>
2019-07-19 18:33:39 +0800

    02-能显示登录组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入路由模块
+// import router from '@/router/index.js'
+import router from '@/router/index.js'
+// 引入公共样式
+import '@/styles/index.less'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login.vue
@@ -0,0 +1,14 @@
+<template>
+ <div id='login'>
+     <p>这是登录组件</p>
+ </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
